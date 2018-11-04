de8830f40be0c92012fb4310e0aa6f97e25495e7
jcable <julian.cable@yahoo.com>
2018-11-04 20:15:20 +0000

    new

---
+++ Formula/faac-drm.rb
@@ -0,0 +1,24 @@
+class FaacDrm < Formula
+  desc "ISO AAC audio encoder with Digital Radio Mondiale support"
+  homepage "https://www.audiocoding.com/faac.html"
+  url "https://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.gz"
+  sha256 "c5141199f4cfb17d749c36ba8cfe4b25f838da67c22f0fec40228b6b9c3d19df"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--without-mp4v2",
+                          "--enable-drm"
+    system "make"
+    mv "libfaac/.libs/libfaac.a", "libfaac/.libs/libfaac_drm.a"
+    mv "libfaac/.libs/libfaac.0.dylib", "libfaac/.libs/libfaac_drm.0.dylib"
+    lib.install "libfaac/.libs/libfaac_drm.a"
+    lib.install "libfaac/.libs/libfaac_drm.0.dylib"
+    lib.install_symlink "libfaac_drm.dylib" => "libfaac_drm.0.dylib"
+  end
+
+  test do
+    assert_predicate lib/"libfaac_drm.a", :exist?
+  end
+end
