4beae3a8ee16c499cba7c08ce58f364ec27dd89a
Julian Cable <julian.cable@yahoo.com>
2025-12-16 19:40:17 +0000

    experiment

--- src/MDI/PacketSocket.cpp
+++ src/MDI/PacketSocket.cpp
@@ -53,8 +53,8 @@ typedef int SOCKET;
 # define SOCKET_ERROR				(-1)
 # define INVALID_SOCKET				(-1)
 #endif
-# include <sys/types.h>
-# include <sys/socket.h>
+//# include <sys/types.h>
+//# include <sys/socket.h>
 # include <unistd.h>
 # include <fcntl.h>
 
